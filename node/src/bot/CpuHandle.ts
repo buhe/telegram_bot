299340d02cffcf1d7095d84719bfe2d582e2d06c
buhe <buhe@netless.io>
2021-01-18 21:56:24 +0800

    refactor bot

---
+++ node/src/bot/CpuHandle.ts
@@ -0,0 +1,14 @@
+import TelegramBot from "node-telegram-bot-api";
+import { exec } from "shelljs";
+export const handleCpu = (bot: TelegramBot ) => {
+    // 'msg' is the received Message from Telegram
+    // 'match' is the result of executing the regexp above on the text content
+    // of the message
+    return (msg: any) =>{
+        const chatId = msg.chat.id;
+        const temp: string = exec("/opt/vc/bin/vcgencmd measure_temp"); // the captured "whatever"
+        bot.sendMessage(chatId, JSON.stringify({ temp }));
+    // send back the matched "whatever" to the chat
+    }
+
+}
--- node/src/bot/index.ts
+++ node/src/bot/index.ts
@@ -2,17 +2,8 @@ import TelegramBot from "node-telegram-bot-api";
 const token = '1564411909:AAFQNbZAskm-aH_-hsjmHjJL8E1Vlr75j74';
 const bot: TelegramBot = new TelegramBot(token, { polling: true });
 import { exec } from "shelljs";
+import { handleCpu } from "./CpuHandle";
 export async function botStart()  {
     // Matches "/echo [whatever]"
-    bot.onText(/\/cpu/, (msg, match) => {
-        // 'msg' is the received Message from Telegram
-        // 'match' is the result of executing the regexp above on the text content
-        // of the message
-
-        const chatId = msg.chat.id;
-        const temp: string = exec("/opt/vc/bin/vcgencmd measure_temp"); // the captured "whatever"
-        bot.sendMessage(chatId, JSON.stringify({ temp }));
-        // send back the matched "whatever" to the chat
-
-    });
+    bot.onText(/\/cpu/, handleCpu(bot));
 };  
