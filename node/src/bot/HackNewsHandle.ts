94501fbad8713715978bbd2b69636acecc7332a2
buhe <buhe@netless.io>
2021-01-21 20:34:59 +0800

    add hack news

--- node/src/bot/HackNewsHandle.ts
+++ node/src/bot/HackNewsHandle.ts
@@ -1,11 +1,13 @@
+import HackerNews from 'hackernews-api-ts';
 import TelegramBot from "node-telegram-bot-api";
-export const handleHelp = (bot: TelegramBot) => {
+export const handleHackews = (bot: TelegramBot) => {
     // 'msg' is the received Message from Telegram
     // 'match' is the result of executing the regexp above on the text content
     // of the message
-    return (msg: any) => {
+    return async (msg: any) => {
         const chatId = msg.chat.id;
-        bot.sendMessage(chatId, '/cpu Cpu 温度\n/sex 一些你懂的小图片\n/github 一些项目\n/weibo 随机返回一个微博热搜\n/news 返回第一个 HackNews');
+        const hn =(await HackerNews.getTopStories(0, 1))[0];
+        bot.sendMessage(chatId, hn.title + ":" + hn.url);
         // send back the matched "whatever" to the chat
     }
 
