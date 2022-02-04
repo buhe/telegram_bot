ec2cdda39aa17c506ec00c989d0b02184d7216dd
buhe <bugu1986@126.com>
2022-02-04 14:22:38 +0800

    Update TestHandle.ts

--- src/bot/schedue/TestHandle.ts
+++ src/bot/schedue/TestHandle.ts
@@ -1,7 +1,7 @@
 import TelegramBot from "node-telegram-bot-api";
 const HomeAssistant = require('homeassistant');
 export const handleTest = (bot: TelegramBot,id: string) => {
-
+    const ha = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiIwN2VhOWEyY2IxYWI0ZGNhOWQ1NmY3NDY2NWI0NjBkMyIsImlhdCI6MTY0Mzk1NDQ0MSwiZXhwIjoxOTU5MzE0NDQxfQ.mg1b0YGMatEs2nHoCoclhwNa4zsMJUySr_dM4onTqtQ";
     
     const hass = new HomeAssistant({
         host: 'http://192.168.31.16',
@@ -17,4 +17,3 @@ export const handleTest = (bot: TelegramBot,id: string) => {
     bot.sendMessage(id, hass.status());
 }
 
-const ha = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiIwN2VhOWEyY2IxYWI0ZGNhOWQ1NmY3NDY2NWI0NjBkMyIsImlhdCI6MTY0Mzk1NDQ0MSwiZXhwIjoxOTU5MzE0NDQxfQ.mg1b0YGMatEs2nHoCoclhwNa4zsMJUySr_dM4onTqtQ";
